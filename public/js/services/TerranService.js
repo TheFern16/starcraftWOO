f2d63ff4e2b1475aeda7a557505d7825a75062e1
Matthew Fernandez <MatthewAFernandez@Matthew-Fernandezs-iMac.local>
2017-01-23 15:41:10 -0800

    added service call structure

---
+++ public/js/services/TerranService.js
@@ -0,0 +1,17 @@
+angular.module('TerranService', []).factory('Terran', ['$http', function($http) {
+
+  return {
+    get: function() {
+      return: $http.get('/api/terran');
+    },
+
+    create: function(terranData) {
+      return $http.post('/api/terran', terranData);
+    },
+
+    delete: function(id) {
+      return $http.delete('/api/nerds/' + id);
+    }
+  }
+
+}]);
