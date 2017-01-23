f9fb8b7dd5d5da247e247a8c2974d2d20c73a64d
Matthew Fernandez <MatthewAFernandez@Matthew-Fernandezs-iMac.local>
2017-01-23 15:29:09 -0800

    added skeleton of main controller

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var mongoose = require('mongoose');
 
 // Shipping out of this port
 
